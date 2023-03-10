a565f3f71f25aab974d4b4e8aaa039011bea430c
Mygal-Alex <magalalecs2003@gmail.com>
2023-03-10 17:09:38 +0200

    home_work-1_Fix

--- home_work-1_advanced/main.js
+++ home_work-1_advanced/main.js
@@ -1,16 +1,16 @@
 document.querySelector(".btn").addEventListener('click', () =>{
     let i = document.querySelector(".input").value;
     if (!(i%2===0) && !((i+1)%3===0)){
-        document.querySelector(".output_1").innerHTML = "низ класик";
+        document.querySelector(".output_1").innerHTML = "Ваше місце: Класичне знизу";
     }
     else if(i%2===0 && !(i%3===0)){
-        document.querySelector(".output_1").innerHTML = "верх класик";
+        document.querySelector(".output_1").innerHTML = "Ваше місце: Класичне зверху";
     }
     else if(!(i%2===0) && (i+1)%3===0){
-        document.querySelector(".output_1").innerHTML = "низ боковуха";
+        document.querySelector(".output_1").innerHTML = "Ваше місце: Бокове знизу";
     }
     else if(i%2===0 && i%3===0){
-        document.querySelector(".output_1").innerHTML = "верх боковуха";
+        document.querySelector(".output_1").innerHTML = "Ваше місце: Бокове зверху";
     }
-    document.querySelector(".output_2").innerHTML = Math.ceil(i/6);
+    document.querySelector(".output_2").innerHTML = "№ Вашого купе :"+Math.ceil(i/6);
 })
