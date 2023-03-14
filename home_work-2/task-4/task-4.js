13172e07d0778e217415a94f413c6a36c4638fb3
Mygal-Alex <magalalecs2003@gmail.com>
2023-03-14 19:20:32 +0200

    home_work-2_task-4 fix

--- home_work-2/task-4/task-4.js
+++ home_work-2/task-4/task-4.js
@@ -1,5 +1,5 @@
 (() => {
-    const arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5];
+    const arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5 ];
     function occurrence(array) {
     return array.sort(
         (a, b) =>
@@ -11,7 +11,7 @@
     const lastIn = last[last.length - 1];
     let us = [];
     for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] === lastIn + 1) {
+    if (arr[i] >= lastIn ) {
         us = arr.splice(arr[i]);
     }
     }
